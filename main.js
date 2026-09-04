b47971ae057a1b791a70059d963731822f613f31
VladimirSA <vladimisav@gmail.com>
2026-09-04 19:14:30 +0600

    minor fixes and adaptive

--- main.js
+++ main.js
@@ -5,25 +5,38 @@ let backSpace = document.querySelector('.number-bs');
 numbers.map((number) => {
     number.addEventListener("click", (event) => {
         switch (event.target.innerText) {
+
             case "AС":
                 display.innerText = "0";
                 break;
+
             case "=":
                 try {
-                    display.innerText = eval(display.innerText);
+                    let result = eval(display.innerText);
+
+                    if (typeof result === 'number' && !Number.isInteger(result)) {
+                        display.innerText = parseFloat(result.toFixed(4));
+                    } else {
+                        display.innerText = result;
+                    }
                 } catch (event) {
-                    display.innerText = "something wrong"
+                    display.innerText = "error";
                 }
                 break;
+
             case "^":
-                    display.innerText += "**";
+                display.innerText += "**";
                 break;
+
             case "²√":
-                    display.innerText = Math.sqrt(display.innerText);
+                let sqrtRes = Math.sqrt(Number(display.innerText));
+                display.innerText = parseFloat(sqrtRes.toFixed(4));
                 break;
+
             case "+/-":
-                    display.innerText = -display.innerText;
+                display.innerText = -display.innerText;
                 break;
+
             case "DEL":
                 if (display.innerText.length <= 1 || display.innerText === "Error") {
                     display.innerText = "0";
@@ -31,6 +44,7 @@ numbers.map((number) => {
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
+                
             default:
                 if (display.innerText === "0" && event.target.innerText !== '.'
                     && event.target.innerText !== '+'
