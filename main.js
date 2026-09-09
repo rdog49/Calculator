591f292df9ceb490c0e568c4dd909a6b87743155
VladimirSA <vladimisav@gmail.com>
2026-09-09 13:43:40 +0600

    fixes for eval structure

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-let display = document.querySelector('.display');
-let numbers = Array.from(document.querySelectorAll('.number'));
-let backSpace = document.querySelector('.number-bs');
+const display = document.querySelector('.display');
+const numbers = Array.from(document.querySelectorAll('.number'));
 
 numbers.map((number) => {
     number.addEventListener("click", (event) => {
         switch (event.target.innerText) {
+            // юзнул штуку которую подсмотрел в гугле для case "nˣ"
+            case "+":
+            case "*":
+            case "/":
+                if (display.innerText === '' || /[+\-*/]$/.test(display.innerText)) {
+                    break;
+                }
+                display.innerText += event.target.innerText;
+                break;
 
+            case "-":
+                if (display.innerText === '') {
+                    display.innerText += '-';
+                    break;
+                }
+
+                if (/[0-9.]$/.test(display.innerText) || /[+\-*/(]$/.test(display.innerText)) {
+                    display.innerText += '-';
+                }
+                break;
+                //-------------------
             case "AС":
-                display.innerText = "0";
+                display.innerText = "";
                 break;
 
             case "=":
@@ -24,40 +43,60 @@ numbers.map((number) => {
                 }
                 break;
 
-            case "^":
-                display.innerText += "**";
-                break;
+            case "nˣ":
+                //честно скажу гугл подсказал как решить
+                const expression = display.innerText;
 
+                if (!expression) {
+                    break;
+                }
+
+                let index = expression.length - 1;
+                while (index >= 0 && /[\d.]/.test(expression[index])) {
+                    index--;
+                }
+
+                const signChar = index >= 0 ? expression[index] : '';
+                const beforeSign = index > 0 ? expression[index - 1] : '';
+
+                if (signChar === '-' && (index === 0 || /[+\-*/(]/.test(beforeSign))) {
+                    const negativeNumber = expression.slice(index);
+                    display.innerText = `${expression.slice(0, index)}(${negativeNumber})**`;
+                } else {
+                    display.innerText += "**";
+                }
+                break;
+                //--------------
             case "²√":
                 let sqrtRes = Math.sqrt(Number(display.innerText));
                 display.innerText = parseFloat(sqrtRes.toFixed(4));
                 break;
 
             case "+/-":
-                display.innerText = -display.innerText;
+                if (display.innerText.includes("-" || "+" || "/" || "*")) {
+                    display.innerText = display.innerText;
+                } else {
+                    display.innerText = -display.innerText;
+                }
                 break;
 
             case "DEL":
                 if (display.innerText.length <= 1 || display.innerText === "Error") {
-                    display.innerText = "0";
+                    display.innerText = "";
                 } else {
                     display.innerText = display.innerText.slice(0, -1);
                 }
                 break;
-                
+
             default:
-                if (display.innerText === "0" && event.target.innerText !== '.'
-                    && event.target.innerText !== '+'
-                    && event.target.innerText !== '*'
-                    && event.target.innerText !== '/') {
+                if (display.innerText === "0" && event.target.innerText !== '.' &&
+                    event.target.innerText !== '+' &&
+                    event.target.innerText !== '*' &&
+                    event.target.innerText !== '/') {
                     display.innerText = event.target.innerText;
-                }
-                else {
+                } else {
                     display.innerText += event.target.innerText;
                 }
         }
     })
 });
-
-
-
